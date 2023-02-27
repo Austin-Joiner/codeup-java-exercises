9cd3627e08d52f500d8580e217bb9dfa9019c380
Austin Joiner <austin.j.joiner1@gmail.com>
2023-02-27 16:25:21 -0500

    added another line of code from codeup.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -19,10 +19,15 @@ public class HelloWorld {
         System.out.println(++x);// now this increments before so instead of 5 it already adds 1 so its 6
         System.out.println(x);// since the above line is already 6 this jst calls on x new vale again which is still 6
 
+        //String theNumberThree = "three";
+        //Object o = theNumberThree;
+        //int three = (int) o;// sends error but allows other outputs.
+        //System.out.println(three);
+
         String theNumberThree = "three";
         Object o = theNumberThree;
-        int three = (int) o;// variable named three isn't being called on.
-
+        int three = (int) "three";
+        System.out.println(three);// still sends an error but doesnt let anything else output
     }
 }
 
