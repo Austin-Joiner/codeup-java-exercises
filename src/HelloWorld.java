bc5fad2184c67315e509e57f89bfd2f46afa8c6a
Austin Joiner <austin.j.joiner1@gmail.com>
2023-02-27 16:06:02 -0500

    error occurs with float and 3.14.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
         System.out.println("World!");
         int myFavoriteNumber = 21;System.out.println(myFavoriteNumber);
         String myString = "My Favorite Number";System.out.println(myString);//doesn't work with char because it's not compatible.
-        long myNumber = 123; System.out.print(myNumber); // with the L removed it gives the same result.
-        // so it seems only whole numbers are able to be used with long not decimal numbers.
+        float myNumber = 3.14;
+        System.out.print(myNumber);
     }
 }
 
