363ef63743b434d4bfa21155c2f4ec0e8e6b5bee
Austin Joiner <austin.j.joiner1@gmail.com>
2023-02-28 16:46:06 -0500

    finished exercise 1a.

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,13 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        //1. Loop Basics
+        //1.a While
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
