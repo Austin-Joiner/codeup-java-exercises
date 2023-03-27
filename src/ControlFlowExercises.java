6ce37e6345d67f2ad3ec905f1f8838c3d41207cb
Austin Joiner <austin.j.joiner1@gmail.com>
2023-03-27 11:15:14 -0400

    1.b question 2 complete.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -12,15 +12,38 @@ public class ControlFlowExercises {
 
 
         //
-        //1.b Do While
-        int i = 0;
+        //1.b Do While question 1
+//        int i = 0;
+//
+//        do {
+//            System.out.println(i);
+//           i = i + 2;
+//
+//
+//        } while (i <= 100); {
+//
+//        }
+
+        //1.b question 2
+//        int i = 100;
+//
+//        do {
+//            System.out.println(i);
+//            i = i - 5;
+//
+//
+//        } while (i >= -10); {
+//
+//        }
+
+        //1.b question 3
+        long i = 2;
 
         do {
             System.out.println(i);
-           i = i + 2;
+            i = i * i;
 
-
-        } while (i <= 100); {
+        } while (i < 1000000); {
 
         }
 
