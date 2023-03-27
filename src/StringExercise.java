3582ef66a259872b7bb7e166c2580726a028592d
Austin Joiner <austin.j.joiner1@gmail.com>
2023-03-27 13:41:07 -0400

    finished part 1 of question 1

---
+++ src/StringExercise.java
@@ -0,0 +1,9 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String makeLine = "We don't need no education \nWe don't need no thought control";
+        System.out.println(makeLine);
+
+
+    }
+}
