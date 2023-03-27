cbe3c410341fc63fea06c5f8d0a529af62af0741
Austin Joiner <austin.j.joiner1@gmail.com>
2023-03-27 13:48:04 -0400

    finished all of question 1

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -4,6 +4,13 @@ public class StringExercise {
         String makeLine = "We don't need no education \nWe don't need no thought control";
         System.out.println(makeLine);
 
+        String sMagic = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(sMagic);
 
+        String cDrive = "In windows, the main drive is usually C:\\";
+        System.out.println(cDrive);
+
+        String backSlash = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(backSlash);
     }
 }
