0cd80711d735cb8c49eb034632edd3687afda1c9
Austin Joiner <austin.j.joiner1@gmail.com>
2023-03-27 14:41:56 -0400

    finished bob talk

---
+++ src/Bob.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner word = new Scanner(System.in);
+        Scanner phrase = new Scanner(System.in);
+        System.out.println("Hey I am BOB, Do you want to talk to me or something?... yes or no bro!");
+        String userChoice = word.nextLine();
+        System.out.println("What?");
+        while (userChoice.equalsIgnoreCase("yes")) {
+
+
+            String userInput = phrase.nextLine();
+
+
+            if (userChoice.equalsIgnoreCase("Yes")) {
+
+
+                if (userInput.endsWith("?")) {
+                    System.out.println("Sure.");
+                } else if (userInput.isEmpty()) {
+                    System.out.println("Fine. Be that way!");
+                } else if (userInput.endsWith("!")) {
+                    System.out.println("Whoa, chill out!");
+                } else if (userInput.equalsIgnoreCase("bye")) {
+                    break;
+                } else {
+                    System.out.println("Whatever.");
+                }
+            }
+
+        }
+        System.out.println("Then Get Out Of My Way...Gosh");
+    }
+}
+
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,6 +1,6 @@
 public class StringExercise {
     public static void main(String[] args) {
-
+// q 1
         String makeLine = "We don't need no education \nWe don't need no thought control";
         System.out.println(makeLine);
 
@@ -12,5 +12,11 @@ public class StringExercise {
 
         String backSlash = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
         System.out.println(backSlash);
+
+        // Q 2
+
+
+
+
     }
 }
