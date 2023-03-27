e55a30c5975ef2dbec9b3d64a82448adf55a51e5
Austin Joiner <austin.j.joiner1@gmail.com>
2023-03-27 10:48:39 -0400

    fixed the first answer so it is on same line and has spaces.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,12 +3,14 @@ public class ControlFlowExercises {
 
         //1. Loop Basics
         //1.a While
-        //
+//
 //        int i = 5;
 //        while (i <= 15) {
-//            System.out.println(i);
+//            System.out.print(i + " ");
 //            i++;
 //        }
+
+
         //
         //1.b Do While
         int i = 0;
